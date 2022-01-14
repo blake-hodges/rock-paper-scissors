10d036c0e62265f4d733d43a5e41e1460ec95900
blake-hodges <tbhodges@gmail.com>
2022-01-14 22:35:52 +0800

    set up basic game functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import React, { useState } from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
+
+
 
 function App() {
     return (
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,32 +1,126 @@
-import { React } from 'react';
+import React, { useState } from 'react';
 import paper from '../assets/paper.png';
 import rock from '../assets/rock.png';
 import scissors from '../assets/scissors.png';
 
 function Main() {
+    const [playerChoice, setPlayerChoice] = useState("rock");
+    const [computerChoice, setComputerChoice] = useState("");
+    const [computerIcon, setComputerIcon] = useState(rock);
+    const [gameMessage, setGameMessage] = useState("Ready to play?");
+    const [activeIcon, setActiveIcon] = useState("")
+    const [playerScore, setPlayerScore] = useState(0);
+    const [computerScore, setComputerScore] = useState(0);
+
+    const resetGame = () => {
+        setPlayerChoice("");
+        setComputerChoice("");
+        setGameMessage("Ready to play again?");
+        setActiveIcon("");
+        setPlayerScore(0);
+        setComputerScore(0);
+    }
+
+    const playGame = () => {
+        let choices = ["rock", "paper", "scissors"];
+        let computerChoice = choices[Math.floor((Math.random() * 3))];
+        if (computerChoice === "rock") {
+            setComputerIcon(rock)
+        } else if (computerChoice === "paper") {
+            setComputerIcon(paper)
+        } else {
+            setComputerIcon(scissors)
+        }
+        setComputerChoice(computerChoice);
+        determineWinner(playerChoice, computerChoice)
+
+    }
+    const determineWinner = (player, computer) => {
+        if (player === "rock" && computer === "paper") {
+            setGameMessage("You lost!")
+            setComputerScore(computerScore => computerScore + 1);
+        }
+        if (player === "rock" && computer === "scissors") {
+            setGameMessage("You won!")
+            setPlayerScore(playerScore => playerScore + 1);
+        }
+        if (player === "rock" && computer === "rock") {
+            setGameMessage("It's a tie!")
+        }
+        if (player === "paper" && computer === "paper") {
+            setGameMessage("It's a tie!")
+        }
+        if (player === "paper" && computer === "scissors") {
+            setGameMessage("You lost!")
+            setComputerScore(computerScore => computerScore + 1);
+        }
+        if (player === "paper" && computer === "rock") {
+            setGameMessage("You won!")
+            setPlayerScore(playerScore => playerScore + 1);
+        }
+        if (player === "scissors" && computer === "paper") {
+            setGameMessage("You won!")
+            setComputerScore(playerScore => playerScore + 1);
+        }
+        if (player === "scissors" && computer === "scissors") {
+            setGameMessage("It's a tie!")
+        }
+        if (player === "scissors" && computer === "rock") {
+            setGameMessage("You lost!")
+            setComputerScore(computerScore => computerScore + 1);
+        }
+
+    }
     return (
         <div className="main">
             <div className="gameCard">
                 <h3>Player One</h3>
-                <p>score: 0</p>
+                <p>score: {playerScore}</p>
                 <div className="icons">
-                    <img src={rock} alt="rock icon" />
-                    <img src={paper} alt="paper icon" />
-                    <img src={scissors} alt="scissors icon" />
+                    <img
+                        src={rock}
+                        className={(activeIcon === "rock") ? 'activeIcon' : ''}
+                        alt="rock icon"
+                        onClick={() => {
+                            setPlayerChoice("rock");
+                            playGame();
+                            setActiveIcon("rock");
+                        }}
+                    />
+                    <img
+                        src={paper}
+                        className={(activeIcon === "paper") ? 'activeIcon' : ''}
+                        alt="paper icon"
+                        onClick={() => {
+                            setPlayerChoice("paper")
+                            playGame();
+                            setActiveIcon("paper");
+                        }}
+                    />
+                    <img
+                        src={scissors}
+                        className={(activeIcon === "scissors") ? 'activeIcon' : ''}
+                        alt="scissors icon"
+                        onClick={() => {
+                            setPlayerChoice("scissors")
+                            playGame();
+                            setActiveIcon("scissors");
+                        }}
+                    />
                 </div>
-                <p>Your choice: Rock</p>
+                <p className="yourChoice">Your choice: {playerChoice}</p>
             </div>
             <div className="gameMessage">
-                <p>You won!</p>
-                <button className="resetButton">Reset</button>
+                <p>{gameMessage}</p>
+                <button className="resetButton" onClick={() => resetGame()}>Reset</button>
             </div>
             <div className="gameCard">
                 <h3>Computer</h3>
-                <p>score: 0</p>
+                <p>score: {computerScore}</p>
                 <div className="icons">
-                    <img src={rock} alt="rock icon" />
+                    <img src={computerIcon} alt="rock icon" />
                 </div>
-                <p>Computer choice: Rock</p>
+                <p className="computerChoice">Computer choice: {computerChoice}</p>
             </div>
         </div>
     )
